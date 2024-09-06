efb983b550a6c384e26f3eed091ba14f41c07696
Dima-kzk <dimakzk@yahoo.com>
2024-09-06 17:10:26 +0300

    init project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello world</h1>");
+});
+
+app.post("/", (request, response) => {
+  response.send({ body: request.body });
+});
+
+app.listen(process.env.PORT || 4000, () =>
+  console.log("server is running on " + process.env.PORT)
+);
