8af6b9684ad58090f9e32d681c69b62cfc39b6e7
Dima-kzk <dimakzk@yahoo.com>
2024-09-10 14:48:31 +0300

    add DB_HOST from env

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const { DB_HOST } = require("./config");
+// const { DB_HOST } = require("./config");
+
+// console.log(process.env.DB_HOST);
+
+const DB_HOST = process.env.DB_HOST;
 
 mongoose.set("strictQuery", true);
 mongoose
