f04f2ca23978332aa778239354e0e8e5308f426b
Dima-kzk <dimakzk@yahoo.com>
2024-09-10 16:07:51 +0300

    fix app.js

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 require("dotenv").config();
 
 const booksRouter = require("./routes/api/books");
 
 const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.use("/api/books", booksRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((error, request, response, next) => {
+  error.message = error.status === undefined ? "Server error" : error.message;
+  const { status = 500, message } = error;
+  response.status(status).json({ message });
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-// const { DB_HOST } = require("./config");
-
-// console.log(process.env.DB_HOST);
-
 const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
